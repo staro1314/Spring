8a3ad997e97768e745a86a06053b87c923df8acf
Staro <staro.fy.zhao@mail.foxconn.com>
2020-06-19 17:28:55 +0800

    添加spi相关demo

--- src/main/java/com/example/spring/Application.java
+++ src/main/java/com/example/spring/Application.java
@@ -1,12 +1,10 @@
 package com.example.spring;
 
 import com.example.spring.domain.annotation.MpkScanner;
-import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-@MapperScan
 @MpkScanner(basePackage = {"com.example.spring.domain.service.serviceImpl"})
 public class Application {
 
